41cb8b5ce86717c7903ac6d750b4d2a657f0428f
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-07-07 14:43:12 -0500

    form updating state

--- flycar-checkout/src/App.tsx
+++ flycar-checkout/src/App.tsx
@@ -16,6 +16,10 @@ import {
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => {
+  const [name, setName] = React.useState("");
+  const [purpose, setPurpose] = React.useState("");
+  const [time, setTime] = React.useState("");
+
   const names: string[] = [
     "Andrew Motz",
     "Andrew Tang",
@@ -25,12 +29,17 @@ export const App = () => {
     "Grace Sun",
   ];
 
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    alert(`${name}`);
+  };
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <ColorModeSwitcher position="absolute" top={4} right={4} />
         <Center minH="100vh" p={3}>
-          <chakra.form justifySelf="center">
+          <chakra.form justifySelf="center" onSubmit={handleSubmit}>
             <VStack
               p={4}
               spacing={8}
@@ -41,7 +50,11 @@ export const App = () => {
               <Heading>Flycar Checkout</Heading>
               <FormControl>
                 <FormLabel>Name</FormLabel>
-                <Select>
+                <Select
+                  name="name"
+                  placeholder="Select an eD"
+                  onChange={(e) => setName(e.target.value)}
+                >
                   {names.map((val, idx) => {
                     return (
                       <option key={idx} value={val}>
@@ -53,7 +66,10 @@ export const App = () => {
               </FormControl>
               <FormControl>
                 <FormLabel>Purpose</FormLabel>
-                <Select>
+                <Select
+                  name="purpose"
+                  onChange={(e) => setPurpose(e.target.value)}
+                >
                   <option value="errand">Errand</option>
                   <option value="detail">Detail</option>
                   <option value="shift">Call</option>
@@ -62,7 +78,11 @@ export const App = () => {
               </FormControl>
               <FormControl>
                 <FormLabel>Time of Return</FormLabel>
-                <Input type="time" />
+                <Input
+                  type="time"
+                  name="time"
+                  onChange={(e) => setTime(e.target.value)}
+                />
               </FormControl>
               <Button type="submit" colorScheme="blue">
                 Submit

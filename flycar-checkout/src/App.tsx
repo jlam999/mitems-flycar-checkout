acb6af1efc8088a42f976a7c759201a0516e10cd
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-07-06 21:52:47 -0500

    basic outline

--- flycar-checkout/src/App.tsx
+++ flycar-checkout/src/App.tsx
@@ -6,33 +6,59 @@ import {
   Link,
   VStack,
   Code,
-  Grid,
+  Flex,
   theme,
+  FormControl,
+  FormLabel,
+  Select,
+  Input,
+  useColorModeValue,
+  Button,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
-export const App = () => (
+export const App = () => {
+  const names: string[] = [
+    "Andrew Motz",
+    "Andrew Tang",
+    "Isabella Carlo",
+    "Sundeep Chakladar",
+    "Eli Mansbach",
+    "Grace Sun"
+  ]
+
+  return (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+      <Flex minH="100vh" p={3} flexDir="column" alignItems="center">
+        <ColorModeSwitcher alignSelf="flex-end" />
+        <form>
+          <VStack p={4} spacing={8} borderWidth={4} borderRadius="lg" w="50vw">
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Select>
+                {names.map((val, idx) => {
+                  return <option key={idx} value={val}>{val}</option>
+                })}
+              </Select>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Purpose</FormLabel>
+              <Select>
+                <option value="errand">Moving Stuff</option>
+                <option value="detail">Detail</option>
+                <option value="shift">Shift</option>
+              </Select>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Time of Return</FormLabel>
+              <Input type="time" />
+            </FormControl>
+            <Button type="submit" colorScheme="blue">Submit</Button>
+          </VStack>
+        </form>
+      </Flex>
     </Box>
   </ChakraProvider>
-)
+)}

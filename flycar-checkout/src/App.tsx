15665cc533d872c2e896a6d3152bc6ba1a24b2fe
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-07-06 22:08:57 -0500

    outline done

--- flycar-checkout/src/App.tsx
+++ flycar-checkout/src/App.tsx
@@ -1,22 +1,19 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Flex,
+  Heading,
   theme,
   FormControl,
   FormLabel,
   Select,
   Input,
-  useColorModeValue,
   Button,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  chakra,
+  Center,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => {
   const names: string[] = [
@@ -25,40 +22,55 @@ export const App = () => {
     "Isabella Carlo",
     "Sundeep Chakladar",
     "Eli Mansbach",
-    "Grace Sun"
-  ]
+    "Grace Sun",
+  ];
 
   return (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Flex minH="100vh" p={3} flexDir="column" alignItems="center">
-        <ColorModeSwitcher alignSelf="flex-end" />
-        <form>
-          <VStack p={4} spacing={8} borderWidth={4} borderRadius="lg" w="50vw">
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <Select>
-                {names.map((val, idx) => {
-                  return <option key={idx} value={val}>{val}</option>
-                })}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Purpose</FormLabel>
-              <Select>
-                <option value="errand">Moving Stuff</option>
-                <option value="detail">Detail</option>
-                <option value="shift">Shift</option>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Time of Return</FormLabel>
-              <Input type="time" />
-            </FormControl>
-            <Button type="submit" colorScheme="blue">Submit</Button>
-          </VStack>
-        </form>
-      </Flex>
-    </Box>
-  </ChakraProvider>
-)}
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <ColorModeSwitcher position="absolute" top={4} right={4} />
+        <Center minH="100vh" p={3}>
+          <chakra.form justifySelf="center">
+            <VStack
+              p={4}
+              spacing={8}
+              borderWidth={4}
+              borderRadius="lg"
+              w="50vw"
+            >
+              <Heading>Flycar Checkout</Heading>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Select>
+                  {names.map((val, idx) => {
+                    return (
+                      <option key={idx} value={val}>
+                        {val}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Purpose</FormLabel>
+                <Select>
+                  <option value="errand">Errand</option>
+                  <option value="detail">Detail</option>
+                  <option value="shift">Call</option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Time of Return</FormLabel>
+                <Input type="time" />
+              </FormControl>
+              <Button type="submit" colorScheme="blue">
+                Submit
+              </Button>
+            </VStack>
+          </chakra.form>
+        </Center>
+      </Box>
+    </ChakraProvider>
+  );
+};
